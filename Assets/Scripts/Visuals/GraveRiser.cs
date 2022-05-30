cc7d0733ded74363fafede6cec0e4738d049fc4b
Spazi <talishmarprkle+g@gmail.com>
2022-05-30 20:57:46 +0300

    GraveRiser component on Grave with TriggerGrave() method

---
+++ Assets/Scripts/Visuals/GraveRiser.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveRiser : MonoBehaviour
+{
+	// This script triggers skeleboi to rise and plays grave particles
+	[SerializeField] ParticleSystem graveParticles;
+	[SerializeField] Animator skeleboiAnim;
+
+	public void triggerGrave()
+	{
+		graveParticles.Play();
+		skeleboiAnim.SetTrigger("Rise");
+	}
+}
