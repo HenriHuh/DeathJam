1788acbe0fa6c215d35004b122e4aaa929f815f9
Spazi <talishmarprkle+g@gmail.com>
2022-05-30 23:21:45 +0300

    Sounds

--- Assets/Scripts/Visuals/GraveRiser.cs
+++ Assets/Scripts/Visuals/GraveRiser.cs
@@ -7,10 +7,15 @@ public class GraveRiser : MonoBehaviour
 	// This script triggers skeleboi to rise and plays grave particles
 	[SerializeField] ParticleSystem graveParticles;
 	[SerializeField] Animator skeleboiAnim;
+	[SerializeField] AudioSource[] graveAudio;
 
 	public void TriggerGrave()
 	{
 		graveParticles.Play();
 		skeleboiAnim.SetTrigger("Rise");
+		foreach (AudioSource audioSource in graveAudio)
+		{
+			audioSource.Play();
+		}
 	}
 }
