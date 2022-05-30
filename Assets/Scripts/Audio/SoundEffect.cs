63e7715c38ec5cbb13328b27f9264b60179588b4
HenriHuh3080 <henkka.huhtala@hotmail.com>
2022-05-30 23:35:12 +0300

    - Sounds disable multiple at same frame

--- Assets/Scripts/Audio/SoundEffect.cs
+++ Assets/Scripts/Audio/SoundEffect.cs
@@ -10,20 +10,38 @@ public class SoundEffect : MonoBehaviour
 	[SerializeField] AudioClip[] randomAudioClips;
 	[SerializeField] float minPitch = 0.8f, maxPitch = 1.2f;
 	[SerializeField] float minRandomPitch = 0.8f, maxRandomPitch = 1.2f;
+
+	static bool firstSFX;
+	static bool secondSFX;
+	static bool thirdSFX;
+
 	void Start()
 	{
 
 	}
 
+    private void Update()
+    {
+		firstSFX = false;
+		secondSFX = false;
+		thirdSFX = false;
+	}
+
 	// Update is called once per frame
 	public void PlayRandomSFX()
 	{
+		if (firstSFX) return;
+		firstSFX = true;
+
 		audioSource.clip = randomAudioClips[Random.Range(0, randomAudioClips.Length)];
 		audioSource.pitch = Random.Range(minRandomPitch, maxRandomPitch);
 		audioSource.Play();
 	}
 	public void PlaySFX(int i)
 	{
+		if (secondSFX) return;
+		secondSFX = true;
+
 		audioSource.clip = audioClips[i];
 		audioSource.pitch = Random.Range(minPitch, maxPitch);
 		audioSource.Play();
@@ -31,6 +49,9 @@ public class SoundEffect : MonoBehaviour
 
 	public void PlayPitchlessSFX(int i)
 	{
+		if (thirdSFX) return;
+		thirdSFX = true;
+
 		audioSource.clip = audioClips[i];
 		audioSource.pitch = 1f;
 		audioSource.Play();
